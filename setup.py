0a65d7fdd6dd4b9fb171724541966386bac753a2
Jordi Puiggené <jp@naralabs.com>
2025-03-20 16:00:27 +0100

    Update code headers

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
 # -*- coding: utf-8 -*-
 #
-# This file is part of BES.TONGA.LIMS
+# This file is part of BES.TONGA.LIMS.
 #
-# Copyright 2023 Beyond Essential Systems Pty. Ltd.
+# BES.TONGA.LIMS is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright 2024-2025 by it's authors.
+# Some rights reserved, see README and LICENSE.
 
 from setuptools import setup, find_packages
 
@@ -11,7 +25,7 @@ version = "1.0.0"
 with open("README.rst", "r") as fh:
     long_description = fh.read()
 
-with open("CHANGELOG.rst", "r") as fh:
+with open("CHANGES.rst", "r") as fh:
     long_description += "\n\n"
     long_description += fh.read()
 
